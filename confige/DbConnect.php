c988b722cf76ab695fbce154ad92c93acc47a499
elcadi <elcadi.mohammad.solicode@gmail.com>
2025-06-21 17:50:14 +0100

    ekfuhwe

--- confige/DbConnect.php
+++ confige/DbConnect.php
@@ -14,7 +14,7 @@ try {
     
     $protected = true; 
     if ($protected && (!isset($_SESSION['login']) || $_SESSION['login'] !== true)) {
-        header("Location: /man9ous/man9ous-/user/connection.php"); 
+        header("Location: /man9ous/man9ous-/user/conection.php"); 
         exit();
     }
 
